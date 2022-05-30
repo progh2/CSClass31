4bdb1b301466bd15cf15350e2dc7e90a4ef0f67d
Gihun Ham <ham@e-mirim.hs.kr>
2022-05-30 15:08:13 +0900

    #4 클래스 인스턴스 변수 C# 인스턴스 변수 초기화가 편하다~

--- CSClass31/Product.cs
+++ CSClass31/Product.cs
@@ -1,9 +1,13 @@
 ﻿namespace CSClass31
 {
-    internal class Product
+    class Product
     {
-        public Product()
+        public string name = "노네임";
+        public int price = 1000 ; 
+
+        public override string ToString()
         {
+            return this.name + ": " + this.price;
         }
     }
 }
--- CSClass31/Program.cs
+++ CSClass31/Program.cs
@@ -50,7 +50,18 @@ namespace CSClass31
             Console.WriteLine(Math.Min(52, 273));
             Console.WriteLine(Math.PI);
 
+            /*
             Product product = new Product();
+            product.name = "고구마";
+            product.price = 6000;
+            */
+            Product productA = new Product() { name = "감자", price = 2000 };
+            Product productB = new Product() { price = 6000, name = "고구마" };
+            Product productC = new Product() { name = "옥수수" };
+
+            Console.WriteLine(productA);
+            Console.WriteLine(productB);
+            Console.WriteLine(productC);
 
         }
     }

fd7c19f8f7ac3b07dd678dd4aedc75b718ae8614
Gihun Ham <ham@e-mirim.hs.kr>
2022-06-13 15:02:38 +0900

    #14 22-8.속성 Property C#의 유용한 문법

--- CSClass31/Program.cs
+++ CSClass31/Program.cs
@@ -130,6 +130,17 @@ namespace CSClass31
 
             Product product = new Product("과자", 1500);
             productA = null;
+
+            Box box = new Box(10, 20);
+            box.Width = 200;
+            box.Height = 100;
+            Console.WriteLine(box.Area);
+
+            box.Width = -10;
+            Console.WriteLine(box.Area);
+            
+            Box box2 = new Box(-10, -20);
+            Console.WriteLine(box2.Area);
         }
     }
 }

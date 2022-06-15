f892cf25aa12c6e708a71d474f404689e0b1ca1a
Gihun Ham <ham@e-mirim.hs.kr>
2022-06-15 10:10:58 +0900

    #19 23-2. 상속 개는 동물이다.

---
+++ CSClass31/Dog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSClass31
+{
+    public class Dog : Animal
+    {
+        string Color;
+
+        public void Bark()
+        {
+            Console.WriteLine("멍멍");
+        }
+    }
+}
--- CSClass31/Program.cs
+++ CSClass31/Program.cs
@@ -146,6 +146,9 @@ namespace CSClass31
             //Console.WriteLine(Fibonacci.Get(10));
             //Console.WriteLine(Fibonacci.Get(30));
             Console.WriteLine(Fibonacci.Get(1000));
+
+            Cat c = new Cat();
+            Dog d = new Dog();
         }
     }
 }
